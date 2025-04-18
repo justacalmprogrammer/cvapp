a74cb27a0390904f75d2fdda5edd3af44857c6f2
justacalmprogrammer <justacalmprogrammer@gmail.com>
2025-04-18 19:10:58 -0300

    aasdf

--- components/experienceg.tsx
+++ components/experienceg.tsx
@@ -6,6 +6,7 @@ const Experienceg = () => {
         <div className="p-6 md:px-12 md:py-44 max-w-5xl mx-auto">
             <Title title="Experiencia" subtitle="" />
             <div className="grid md:grid-cols-2 gap-8 mt-5">
+                <p>asñdlkfj</p>
             </div>
         </div>);
 }
